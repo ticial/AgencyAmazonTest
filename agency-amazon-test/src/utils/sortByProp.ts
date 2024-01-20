e5024be2aac347b012903c6f81f2a6999e7b142c
ticial <49561842+ticial@users.noreply.github.com>
2024-01-20 12:03:43 +0200

    fix bug

--- agency-amazon-test/src/utils/sortByProp.ts
+++ agency-amazon-test/src/utils/sortByProp.ts
@@ -2,27 +2,22 @@ export function sortStrings(a: string, b: string) {
   const propA = a.toLowerCase();
   const propB = b.toLowerCase();
 
-  if (propA < propB) {
-    return -1;
-  }
-  if (propA > propB) {
-    return 1;
-  }
+  if (propA < propB) return -1;
+  if (propA > propB) return 1;
   return 0;
 }
 
 export function sortByProp(prop: string, array: Record<string, string | number>[]) {
   const descending = prop.startsWith("-");
   if (descending) prop = prop.slice(1);
-
-  if (array.length < 0) return array;
-  const first = array[0];
-  if (typeof first[prop] === "string") {
+  if (array.length === 0) return array;
+  const valueType = typeof array[0][prop];
+  if (valueType === "string") {
     return descending
       ? array.sort((a, b) => sortStrings(b[prop] as string, a[prop] as string))
       : array.sort((a, b) => sortStrings(a[prop] as string, b[prop] as string));
   }
-  if (typeof first[prop] === "number") {
+  if (valueType === "number") {
     return descending
       ? array.sort((a, b) => (b[prop] as number) - (a[prop] as number))
       : array.sort((a, b) => (a[prop] as number) - (b[prop] as number));
